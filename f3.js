19b495c2f5961785d530777e1ca3d5fc644acf63
Prakhar227 <127192867+Prakhar227@users.noreply.github.com>
2023-04-03 18:14:50 +0530

    Add files via upload

---
+++ f3.js
@@ -0,0 +1,9 @@
+const proceed = document.querySelector('#proceed')
+proceed.addEventListener('click', (e) =>{
+    location.href="quiz.html"
+})
+
+const create = document.querySelector('#create')
+create.addEventListener('click', (e)=>{
+    location.href = "create.html"
+})
